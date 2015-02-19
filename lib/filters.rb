1e74a5c721c1effaad64be45196995050c87273a
itgsod-omer-akgun <omer.akgun@elev.it-gymnasiet.se>
2015-02-19 15:22:52 +0100

    klar med filter

--- lib/filters.rb
+++ lib/filters.rb
@@ -1,5 +1,11 @@
 def filter(array:, value:)
-  array.each do
+  array2 = []
+  array.each do |x|
+    if x == value
+      array2 << value
+    end
+  end
+  return array2
 end
 
-filter(array: [1, 2, 3, 3, 3], value = 3)
+p filter(array: [1, 2, 3, 3, 3], value: 3)
